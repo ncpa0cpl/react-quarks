e41cc6341d7b4c1c29bf6d68f5143a6c6db7656f
ncpa0cpl <sbretner@touchbistro.com>
2023-01-30 15:46:45 +0100

    feat: immut state middleware

---
+++ src/Middlewares/ImmutableStateMiddleware/ImmutableStateMiddleware.ts
@@ -0,0 +1,42 @@
+import type { QuarkMiddleware } from "../../Types";
+
+const freezeDeep = (obj: any) => {
+  if (
+    obj !== null &&
+    (typeof obj === "object" || typeof obj === "function") &&
+    !Object.isFrozen(obj)
+  ) {
+    if (Array.isArray(obj)) {
+      obj.forEach(freezeDeep);
+    } else {
+      Object.getOwnPropertyNames(obj).forEach((prop) => {
+        freezeDeep(obj[prop]);
+      });
+    }
+
+    Object.freeze(obj);
+  }
+
+  return obj;
+};
+
+export const createImmutableStateMiddleware = (): QuarkMiddleware<
+  any,
+  undefined
+> => {
+  return (_, action, resume) => {
+    if (
+      typeof action === "object" &&
+      action !== null &&
+      !(action instanceof Promise)
+    ) {
+      if (Object.isFrozen(action)) {
+        return resume(action);
+      }
+
+      return resume(freezeDeep(action));
+    }
+
+    return resume(action);
+  };
+};
