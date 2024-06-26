daee620d878bb7fd785652b36920b9185abb84ac
ncpa0cpl <32438334+ncpa0cpl@users.noreply.github.com>
2024-06-26 16:49:18 +0200

    feat: small selector hook improvements

--- src/Utilities/UseCachedSelector.ts
+++ src/Utilities/UseCachedSelector.ts
@@ -1,48 +1,69 @@
-import { isEqual } from "lodash";
 import React from "react";
 import type { QuarkContext, QuarkSelector } from "../Types";
 
 const NULL_SYM = Symbol("null");
+
+const DEFAULT_PORTAL = () => ({
+  cache: {
+    prevState: NULL_SYM as any,
+    prevArgs: [] as any,
+    prevResult: NULL_SYM as any,
+  },
+  selectorImpl: ((v: any) => v) as any,
+  args: [] as any,
+});
 
 export const useCachedSelector = <T, ARGS extends any[], R>(
   selector: QuarkSelector<T, ARGS, R>,
   self: QuarkContext<T, any>,
   latestArgs: ARGS,
 ): () => R => {
-  const portal = React.useRef({
-    cache: {
-      prevState: NULL_SYM as typeof NULL_SYM | T,
-      prevArgs: [] as any as ARGS,
-      prevResult: NULL_SYM as typeof NULL_SYM | R,
+  const [portal] = React.useState(
+    DEFAULT_PORTAL as any as {
+      cache: {
+        prevState: typeof NULL_SYM | T;
+        prevArgs: ARGS;
+        prevResult: typeof NULL_SYM | R;
+      };
+      selectorImpl: typeof selector;
+      args: typeof latestArgs;
     },
-    selectorImpl: selector,
-    args: latestArgs,
-  });
+  );
 
-  portal.current.args = latestArgs;
-  portal.current.selectorImpl = selector;
+  portal.args = latestArgs;
+  portal.selectorImpl = selector;
 
   const [selectorWrapper] = React.useState(() => () => {
-    const { args, selectorImpl } = portal.current;
-    const { prevArgs, prevResult, prevState } = portal.current.cache;
+    const { args, selectorImpl } = portal;
+    const { prevArgs, prevResult, prevState } = portal.cache;
     const state = self.value;
 
     if (
       prevResult === NULL_SYM
-      || state !== prevState
-      || !isEqual(args, prevArgs)
+      || !Object.is(state, prevState)
+      || !compareArrays(args, prevArgs)
     ) {
       const result = selectorImpl(state, ...args);
 
-      portal.current.cache = {
+      portal.cache = {
         prevState: state,
         prevArgs: args,
         prevResult: result,
       };
     }
 
-    return portal.current.cache.prevResult as R;
+    return portal.cache.prevResult as R;
   });
 
   return selectorWrapper;
 };
+
+function compareArrays<T>(a: T[], b: T[]): boolean {
+  if (a.length !== b.length) return false;
+
+  for (let i = 0; i < a.length; i++) {
+    if (!Object.is(a[i], b[i])) return false;
+  }
+
+  return true;
+}
