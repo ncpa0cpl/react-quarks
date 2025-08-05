9dbec8a3218327e5ab10eafdec1982269c1fc6ce
ncpa0cpl <szymonb21@gmail.com>
2025-08-05 16:07:04 +0200

    ci: fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,9 +13,17 @@ git config user.email github-actions@github.com
 if [ "$issemver" -eq "1" ]; then
     if ! [ "$currenttag" = "\"$TAG_NAME\"" ]; then
         echo "Branch tag is different than packge.json version. Updating package.json version to $TAG_NAME"
+
+        INIT_HEAD=$(git describe --exact-match --tags)
+        git fetch origin master --depth 1
+        git switch master
         npm version "$TAG_NAME"
         git push --no-verify
+
+        git checkout "tags/$INIT_HEAD"
+        npm version "$TAG_NAME"
     fi
+
     echo "Publishing to npm"
     npm publish
 fi
